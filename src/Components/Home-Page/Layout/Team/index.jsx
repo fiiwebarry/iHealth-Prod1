ea783fb634ec4936d5afc972eb1abc5805cf29b2
fiiwebarry <fiiwebarry@yahoo.com>
2023-07-14 23:09:47 +0100

    update on text decription mobile view

--- src/Components/Home-Page/Layout/Team/index.jsx
+++ src/Components/Home-Page/Layout/Team/index.jsx
@@ -16,6 +16,7 @@ const Team = () => {
         </div>
         <div className="team-join-card">
           <div className="team-join-card-inner">
+
             <h2 className="team-join-heading">Join NF Community</h2>
             <p className="team-join-slogan">
               Connect with other people. Share your story.
@@ -31,6 +32,9 @@ const Team = () => {
               Join
             </a>
           </div>
+          <p className="team-join-description">As a community member, you'll come together with others living with your condition to support each other,learn
+            from each other, inspire each other, to feel a sense of fellowship, safety and belonging together.
+          </p>
         </div>
       </div>
     </section>
